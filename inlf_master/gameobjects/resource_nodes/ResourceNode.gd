bf607219e97b6f76690780490b79813ca16f2bd6
ConnorJ <connorjernigan@msn.com>
2023-12-20 11:52:53 -0600

    small fixes
    updated hp values for many items and player, added damage type, fixed muzzle_flash on shotgun

--- inlf_master/gameobjects/resource_nodes/ResourceNode.gd
+++ inlf_master/gameobjects/resource_nodes/ResourceNode.gd
@@ -3,6 +3,7 @@ extends Interactable
 export(Resource) var slot_data #resource item to drop on death
 export(String) var display_name = "NULL" #what to display on nameplate
 export(Array, Damage.DamageType) var blocked_damage_types
+export(bool) var punching_hurts = false
 
 onready var health = $Health
 onready var name_plate = $CanvasLayer/Info/VBoxContainer/Label
@@ -12,6 +13,7 @@ var item_prefab = preload("res://item/pick_up/Pickup.tscn")
 var dead = false
 
 func _ready():
+	randomize()
 	health.init()
 	health.connect("dead", self, "on_death")
 	health.connect("health_changed", prog_bar, "update_bar")
@@ -28,10 +30,19 @@ func on_death(): #spawn item, delete self
 	Globals.create_pickup(slot_data, self)
 	queue_free() #destroy
 
-# change to: on_hurt(damage, dealer = null) this is because the dealer may not be passed, passed only for player
 func on_hurt(damage):
 	if dead:
 		return
-	# match(damage.damage_type) for each type, block damage X% of the time
-	# if the damage type is fists, 50% of the time block it, then also always deal damage to the dealer/player
-	health.hurt(damage.amount)
+	var random_result = randf()
+	for damage_type in blocked_damage_types:
+		if damage.type == Damage.DamageType.Fists:
+			if punching_hurts:
+				Globals.current_player.on_hurt(damage) #replace with damage from this object
+			health.hurt(damage.amount) #always damage the resource_node with fists, but hurt player
+			return
+		elif damage.type == damage_type:
+			if random_result < 0.5: #fifty percent chance to deal damage to object
+				health.hurt(damage.amount)
+				return
+		else:
+			health.hurt(damage.amount) #deal damage to object
