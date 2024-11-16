a874daffec0d8f623b67801df65489bc7c217b07
Connor <connorjernigan@msn.com>
2024-11-16 17:37:15 -0600

    changed game desktop icon
    removed unused gameobjects

--- inlf_master/scenes/menu/menu.gd
+++ inlf_master/scenes/menu/menu.gd
@@ -7,7 +7,7 @@ signal starting(level_name: String)
 @onready var level_select: Control = $LevelSelect
 
 func _ready():
-	Input.set_mouse_mode(Input.MOUSE_MODE_CONFINED)
+	Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
 	ScreenFader.fade_in()
 	level_select.connect("selected_level", Callable(self, "level_selected"))
 
@@ -23,15 +23,6 @@ func level_selected(level_name: String):
 
 func _on_StartButton_button_down():
 	level_select.visible = true
-	
-	#ScreenFader.fade_out()
-	#start_fx.play()
-	#starting.emit()
-	#button_press.play()
-	
-	#await start_fx.finished
-	
-	#queue_free()
 
 func _on_ExitButton_pressed():
 	get_tree().quit()
